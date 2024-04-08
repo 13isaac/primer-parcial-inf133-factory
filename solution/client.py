adfef53715293a6c1709866659f952bc4c9fb560
13isaac <115235220+13isaac@users.noreply.github.com>
2024-04-08 17:46:29 -0100

    Entrega Final

--- solution/client.py
+++ solution/client.py
@@ -11,7 +11,7 @@ compra_new={
     "order_type":"Fisica"
 }
 response_new=requests.post(url+"orders",json=compra_new)
-print("--añadido--")
+#print("--añadido--")
 print(response_new.text)
 
 #segundi post
@@ -26,5 +26,44 @@ compra_new={
 }
 
 response_new=requests.post(url+"orders",json=compra_new)
-print("--añadido--")
+#print("--añadido--")
 print(response_new.text)
+
+#mostrar todo
+response=requests.get(url+"orders")
+#print("---lilsta de ordenes---")
+print(response.text)
+
+#filtrar por estado
+response_est=requests.get(url+"orders/?status=Pendiente")
+#print("---filtrando por estado---")
+print(response_est.text)
+
+#actualizar por id
+status_a={"status":"En proceso"}
+response_a=requests.put(url+"orders/1",json=status_a)
+#print("---actualizar por id---")
+print(response_a.text)
+
+#borrar por id
+response_del=requests.delete(url+"orders/1")
+#print("---eliminado por id---")
+print(response_del.text)
+
+#agregar una nueva orden
+compra_new={
+    "client":"Ana Gutierrez",
+    "status":"Pendiente",
+    "payment":"Tarjeta de Debito",
+    "shipping":20.0,
+    "products":["Licuadora","Refrigeradora","Lavadora"],
+    "order_type":"Fisica"
+}
+response_new=requests.post(url+"orders",json=compra_new)
+#print("---añadido---")
+print(response_new.text)
+
+#lista de ordenes
+response=requests.get(url+"orders")
+#print("---lilsta de ordenes---")
+print(response.text)
